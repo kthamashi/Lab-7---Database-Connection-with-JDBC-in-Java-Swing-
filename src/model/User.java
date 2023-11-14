2e505e37cdfdd25b36db6994c5bcb92d9cd58e4f
Thamashi Kumarasinghe <kumarasinghe.t@northeastern.edu>
2023-11-13 21:36:29 -0500

    Java swing application with crud operations

---
+++ src/model/User.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author thamashikumarasinghe
+ */
+public class User {
+    private String name;
+    private int age;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+    
+}
